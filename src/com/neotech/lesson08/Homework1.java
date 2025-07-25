3d526e609899ab9e03dbed36731a0e1648865af2
Mai <mai.y.malanog@gmail.com>
2025-07-25 20:37:34 +0900

    2nd commit from Eclipse project

--- src/com/neotech/lesson08/Homework1.java
+++ src/com/neotech/lesson08/Homework1.java
@@ -1,5 +1,10 @@
 package com.neotech.lesson08;
 
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+
 import com.neotech.utils.BaseClass;
 
 public class Homework1 extends BaseClass {
@@ -15,8 +20,53 @@ public class Homework1 extends BaseClass {
 	6) Quit all browsers
 	*/
 
-	public static void main(String[] args) {
-		// https://neotech.vercel.app/email
+	public static void main(String[] args) throws InterruptedException {
+	// https://neotech.vercel.app/email
+		
+		setUp();
+		
+		// locate and click on the Click here link
+		driver.findElement(By.linkText("Click Here")).click();
+		
+		// Will be opening 2 windows so we need to focus what window will be handled.
+		Set <String> handles = driver.getWindowHandles();
+		
+		Iterator <String> it = handles.iterator();
+		String parentWindow = it.next();
+		String childWindow = it.next();
+		
+		// we have to switch into the child window
+		driver.switchTo().window(childWindow);
+		
+		// Enter the email in child window
+		driver.findElement(By.id("emailid")).sendKeys("test@gmail.com");
+		
+		// Another way to find: //input[@name='emailid']
+		// driver.findElement(By.xpath("//input[@name='emailid']")).sendKeys("test@gmail.com");
+		
+		Thread.sleep(3000);
+		
+		// click on the button
+		driver.findElement(By.name("btnLogin")).click();
+		
+		Thread.sleep(3000);
+		
+		// get the credential from the page
+		String username = driver.findElement(By.id("username")).getText();
+		String password = driver.findElement(By.id("password")).getText();
+		
+		System.out.println("Username is: " + username);
+		System.out.println("Password is: " + password);
+		
+		// Close the child window, switch back to the main window
+		driver.close(); // = active window = child window
+		// driver.switchTo().window(parentWindow); = NO need to switch but 
+		
+		Thread.sleep(3000);
+			
+		tearDown();
+		
+		
 		
 		
 		
---
+++ src/com/neotech/lesson08/Homework2.java
@@ -0,0 +1,20 @@
+package com.neotech.lesson08;
+
+public class Homework2 {
+	
+	// Part 2-1
+	
+	/*	 
+	1) Launch the browser and open the site "https://neotech.vercel.app/iframe/multi2"
+	2) Verify on the page header "Not a Friendly Topic" displayed
+	3) Click on the Inner Frame Check box
+	4) Choose Dog from Animals dropdown
+	5) Quit the browser
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
